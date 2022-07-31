002de03797b8fac7562c89f8b86989508e6a4f9b
Timothée Fermeaux <fermeaux.t@gmail.com>
2022-07-31 19:01:22 +0200

    feat: remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import {
-  Routes,
-  Route
-} from 'react-router-dom'
 import HabitPage from './habit'
 
 export default function App () {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<HabitPage />} />
-      </Routes>
+      <HabitPage />
     </div>
   )
 }
