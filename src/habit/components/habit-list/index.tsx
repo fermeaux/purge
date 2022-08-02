741947068fceeb62654051a333c54c44fb66cc1f
Timothée Fermeaux <timothee.fermeaux@ogury.co>
2022-08-03 00:08:42 +0200

    feat: make it better responsive

--- src/habit/components/habit-list/index.tsx
+++ src/habit/components/habit-list/index.tsx
@@ -4,7 +4,7 @@ import HabitItem from '../habit-item'
 
 export default function HabitList () {
   return (
-    <section className='grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+    <section className='grid gap-4 sm:grid-cols-2 sm:gap-8 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
       {HABITS.map(habit => <HabitItem key={habit.id} habit={habit} />)}
     </section>
   )
--- src/habit/index.tsx
+++ src/habit/index.tsx
@@ -3,9 +3,9 @@ import HabitList from './components/habit-list'
 
 export default function HabitPage () {
   return (
-    <main className='py-8 px-16'>
+    <main className='py-4 px-8 sm:py-8 sm:px-16'>
       <h1 className='text-center text-3xl font-bold'>Traqueur d{"'"}addiction</h1>
-      <div className='my-8'>
+      <div className='my-4 sm:my-8'>
         <HabitList />
       </div>
     </main>
